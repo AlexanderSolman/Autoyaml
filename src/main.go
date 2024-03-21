0896802960b90a7797c134c84be2e1d3881fa8d6
AlexanderSolman <alexander.solman.andersson@gmail.com>
2024-03-21 19:47:52 +0100

    Implemented calls for file creation

--- src/main.go
+++ src/main.go
@@ -20,22 +20,29 @@ func main() {
     }
 
     command := strings.ToLower(os.Args[1])
+    flag := os.Args[2]
+    filePath := os.Args[3] // Should be absolute or defaults current directory (name.yaml has to be set)
+    err := validFileName(filePath)
+    if err != nil {
+       fmt.Println(err)
+    }
+    if flag != "-a" || flag != "" { fmt.Printf("Invalid flag: %s, defaults as blank", flag) }
 
     switch command {
     case "deployment":
-        
+        createDeployment(&flag, filePath)
     case "serviceaccount":
-
+        createSeriveAccount(&flag, filePath)
     case "role":
-
+        createRole(&flag, filePath)
     case "rolebinding":
-
+        createRoleBinding(&flag, filePath)
     case "clusterrole":
-    
+        createClusterRole(&flag, filePath)
     case "clusterrolebinding":
-    
+        createClusterRoleBinding(&flag, filePath)
     case "pod":
-
+        createPod(&flag, filePath)
     default:
         fmt.Println("Command does not match a valid Kubernetes resource")
         os.Exit(1)
