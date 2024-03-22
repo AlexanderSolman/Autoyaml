56fbae1bf4410a87c13b8db871de70bdc7913006
AlexanderSolman <alexander.solman.andersson@gmail.com>
2024-03-22 14:32:23 +0100

    Fixed bug, adjusted command handling

--- src/main.go
+++ src/main.go
@@ -11,24 +11,46 @@ import (
 // >> autoyaml <kind(deployment)> <flag(-b(basic)|-a(advanced))> <file/path/your.yaml(defaults to current directory ./your.yaml)>
 
 func main() {
-    var err error
-    if len(os.Args) < 2 {
+    var (
+        err error
+        flag string
+        filePath string
+    )
+    if len(os.Args) < 3 {
         fmt.Println("Not enough arguments")
         os.Exit(1)
-    } else if len(os.Args) > 3 {
+    } else if len(os.Args) > 4 {
         fmt.Println("Too many arguments")
         os.Exit(1)
     }
 
+    amountOfArguments := len(os.Args)
+
     command := strings.ToLower(os.Args[1])
-    flag := os.Args[2]
-    filePath := os.Args[3] // Should be absolute or defaults current directory (name.yaml has to be set)
+    if amountOfArguments == 4 { 
+        flag = os.Args[2]
+        if flag != "-a" {
+            var i string
+            fmt.Printf("Invalid flag: %s. Did you mean -a? [y,n]", flag)
+            fmt.Scanln(&i)
+            if i == "y" { 
+                flag = "-a" 
+            } else { 
+                flag = ""
+                fmt.Println("Flag is set to deafult")
+            }
+        }
+        filePath = os.Args[3] // Should be absolute or defaults current directory (name.yaml has to be set)
+    } else {
+        flag = ""
+        filePath = os.Args[2]
+    }
+    
     err = validFileName(filePath)
     if err != nil {
         fmt.Println(err)
         os.Exit(1)
     }
-    if flag != "-a" || flag != "" { fmt.Printf("Invalid flag: %s, defaults as blank", flag) }
 
     err = createFile(&flag, filePath, command)
     if err != nil {
